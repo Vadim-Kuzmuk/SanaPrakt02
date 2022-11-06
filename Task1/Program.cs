9653a5aae92bbb913ecd78efc02cfa689652962b
Vadim Kuzmuk <ipzm221_kvo@student.ztu.edu.ua>
2022-11-06 20:56:42 +0200

    change 1-3 line

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Змінив текст");
+Console.WriteLine("повідомлення у");
+Console.WriteLine("перших 3х рядках");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
